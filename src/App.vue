bde19bfb85f9ed17c26687405a6c293a12cb7f67
demaxl <aweabdullahi@gmail.com>
2024-05-23 18:43:51 +0100

    Finished header of main page

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container mt-5">
+        <div class="header row gap-4">
+            <div class="col">
+                <h1 class="bold">Good Morning, Demaxl!👋</h1>
+                <h3 class="text-muted mt-2">Today, Wed 6 July 2023</h3>
+            </div>
+            <div class="col-md-6">
+                <div class="d-flex date-container justify-content-md-end">
+                    <div class="date d-flex align-items-center bg-light p-3 rounded-3 me-2 my-auto">
+                        <span
+                            class="chevron rounded-3 p-1 bold"
+                            uk-icon="icon: chevron-down; ratio: 1.5"
+                        ></span>
+                        <span class="ps-3 pe-5 bold">Today</span>
+                    </div>
+                    <div class="bg-light p-3 rounded-3 d-flex align-items-center">
+                        <span uk-icon="icon: menu; ratio: 1.5"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+$background-color: #f4f4f4;
+// $background-color: #eee;
+
+* {
+    box-sizing: border-box;
+}
+
+html,
+body {
+    font-family: 'Poppins', sans-serif;
+    background-color: $background-color;
+}
+
+.bold {
+    font-weight: bold;
+}
+
+.header {
+    .date-container {
+        height: 50px !important;
+    }
+    .date {
+        height: 50px;
+        .chevron {
+            // background-color: #eee;
+            background-color: $background-color;
+            // width: 30px;
+            // height: 30px;
+            // padding: 10px;
+            // font-size: 40px;
+        }
+    }
+}
+</style>
